f60ac9c6ce43f85641370e77fc831759d22f8cfe
pencte <zakyyulyansyah@gmail.com>
2026-04-20 14:25:47 +0700

    Update page.tsx

--- src/app/player/growid/login/dashboard/page.tsx
+++ src/app/player/growid/login/dashboard/page.tsx
@@ -3,7 +3,7 @@
 import { useSession } from "next-auth/react";
 
 export default function DashboardPage() {
-  const { data: session } = useSession();
+  const { data: session } = useSession() || {};
 
   return (
     <div style={{
